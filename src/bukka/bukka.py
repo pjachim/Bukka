abf5f7ecfcc04667cca3561493b605a3e8effc90
Peter Jachim <pjachim@outlook.com>
2025-11-15 22:26:20 -0600

    Created initial CLI.

--- src/bukka/bukka.py
+++ src/bukka/bukka.py
@@ -1,5 +1,23 @@
-def main():
-    ...
+import argparse
+from bukka.logistics.files.file_manager import FileManager
 
-if __name__ == 'main':
-    main()
+def main(name, dataset):
+    file_manager = FileManager(
+        name,
+        orig_dataset=dataset
+    )
+
+    file_manager.build_skeleton()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--name', '-n', type=str, required=True)
+    parser.add_argument('--dataset', '-d', type=str, required=False, default=None)
+
+    args = parser.parse_args()
+
+    main(
+        name=args.name,
+        dataset=args.dataset
+    )
