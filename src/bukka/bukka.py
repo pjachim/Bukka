e3066e734d83d7ad025ac699a6fc2d17f3db9721
Peter Jachim <pjachim@outlook.com>
2025-11-16 22:56:03 -0600

    Added logger.

--- src/bukka/bukka.py
+++ src/bukka/bukka.py
@@ -1,7 +1,12 @@
 import argparse
 from bukka.logistics.project import Project
+from bukka.utils import bukka_logger
+
+logger = bukka_logger.BukkaLogger(__name__)
 
 def main(name, dataset):
+    logger.info('Creating Bukka project!', format_level='h1')
+    
     proj = Project(
         name,
         dataset_path=dataset
