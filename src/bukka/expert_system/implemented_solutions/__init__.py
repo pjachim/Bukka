85f824f2e9af383a866c9702f1d40d270b0018f7
Peter Jachim <pjachim@outlook.com>
2025-11-22 23:40:29 -0600

    Made estimators easier to fetch.

--- src/bukka/expert_system/implemented_solutions/__init__.py
+++ src/bukka/expert_system/implemented_solutions/__init__.py
@@ -2,3 +2,6 @@ from . import outlier_solutions
 from . import categorical_solutions
 from . import null_solutions
 from . import text_solutions
+from . import classification_solutions
+from . import regression_solutions
+from . import clustering_solutions
