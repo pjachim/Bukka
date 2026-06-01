f9cda2c8dbee33f26a2b3a1534e0f0551cdc17ce
Peter Jachim <pjachim@outlook.com>
2026-06-01 00:02:07 -0500

    Bumped version number.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'Bukka'
 copyright = '2026, Peter Jachim'
 author = 'Peter Jachim'
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
